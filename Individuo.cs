9f7544d45829d520994ad33fecf23737f8373536
isabella-66 <amasprog@gmail.com>
2022-04-07 01:29:41 -0300

    Atualizações Individuo e Program para POO

--- Individuo.cs
+++ Individuo.cs
@@ -1,9 +1,65 @@
 public class Individuo
 {
     public string Nome { get; set; }
-    public float Altura { get; set; }
-    public float Peso { get; set; }
-    public float Imc { get => Peso / (Altura * Altura); }
+    public double Altura { get; set; }
+    public double Peso { get; set; }
+    public double Imc { get => Peso / (Altura * Altura); }
+
+    public Individuo (string nome, double altura, double _peso)
+    {
+        Nome = nome;
+        Altura = altura;
+        Peso = _peso;
+    }
     
-    
+    public string FaixaPeso() 
+    {
+        if (Imc < 17.00)
+        {
+            return "muito abaixo do peso.";
+        } 
+        else if (Imc >= 17.00 && Imc < 18.50)
+        {
+            return "abaixo do peso.";
+        }
+        else if (Imc >= 18.50 && Imc < 25.00)
+        {
+            return "peso normal.";
+        }
+        else if (Imc >= 25.00 && Imc < 30.00)
+        {
+            return "acima do peso.";
+        }
+        else if (Imc >= 30.00 && Imc < 35.00)
+        {
+            return "Obesidade I.";
+        }
+        else if (Imc >= 35.00 && Imc < 40.00)
+        {
+            return "Obesidade II (severa).";
+        }
+        else 
+        {
+            return "Obesidade III (mórbida).";
+        }
+    }
+
+    public string Situacao()
+    {
+        double aux = 0.00;
+        double peso = 0.00;
+
+        if (Imc < 18.5) 
+        {
+            peso = 18.5 * Altura * Altura;
+            aux = peso - Peso;
+            return $"precisa ganhar {aux}kg";
+        }
+        else if (Imc > 24.99)
+        {
+            peso = 24.99 * Altura * Altura;
+            aux = Peso - peso;
+            return $"precisa perder {aux}kg";
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -7,49 +7,23 @@ namespace CalculadoraIMC
         static void Main(string[] args)
         {
             Console.WriteLine("Qual o seu nome?");
-            string nome = Console.ReadLine();
+            string Nome = Console.ReadLine();
 
             Console.WriteLine("Qual a sua altura em metros? Ex.: 1,65");
-            float altura = Convert.ToSingle(Console.ReadLine());
+            double Altura = Convert.ToSingle(Console.ReadLine());
 
             Console.WriteLine("Qual o seu peso em kg? Ex.: 70,3");
-            float peso = Convert.ToSingle(Console.ReadLine());
+            double Peso = Convert.ToSingle(Console.ReadLine());
 
-            
+            double Imc = Math.Round(Peso / (Altura * Altura), 2);
 
-            double imc = Math.Round(peso / (altura * altura), 2);
+            Console.WriteLine("{0}, sua altura é {1}m e seu peso, {2}kg.", Nome, Altura, Peso);
+            Console.WriteLine("Então, seu IMC é {0}", Imc);
 
-            Console.WriteLine("{0}, sua altura é {1}m e seu peso, {2}kg.", nome, altura, peso);
-            Console.WriteLine("Então, seu IMC é {0}", imc);
+            Individuo i1 = new Individuo(Nome, Altura, Peso);
 
-            if (imc < 17.00)
-            {
-                Console.WriteLine("Você está muito abaixo do peso.");
-            } 
-            else if (imc >= 17.00 && imc < 18.50)
-            {
-                Console.WriteLine("Você está abaixo do peso.");
-            }
-            else if (imc >= 18.50 && imc < 25.00)
-            {
-                Console.WriteLine("Você está com peso normal.");
-            }
-            else if (imc >= 25.00 && imc < 30.00)
-            {
-                Console.WriteLine("Você está acima do peso.");
-            }
-            else if (imc >= 30.00 && imc < 35.00)
-            {
-                Console.WriteLine("Você está com Obesidade I.");
-            }
-            else if (imc >= 35.00 && imc < 40.00)
-            {
-                Console.WriteLine("Você está com Obesidade II (severa).");
-            }
-            else 
-            {
-                Console.WriteLine("Você está com Obesidade III (mórbida).");
-            }
+            Console.WriteLine("Você está " + i1.FaixaPeso());
+            Console.WriteLine("Você " + i1.Situacao);
         }
     }
 }
